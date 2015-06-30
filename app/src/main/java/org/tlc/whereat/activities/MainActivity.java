e6e359f48d8030390da219087bf0e2523908df75
aguestuser <aguestuser@riseup.net>
2015-06-29 21:34:00 -0400

    Go button retrieves location * Clicking go button toasts location * Long-clicking go button toasts location every 15 sec * Long-clicking go button again turns off location polling

--- app/src/main/java/org/tlc/whereat/activities/MainActivity.java
+++ app/src/main/java/org/tlc/whereat/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package org.tlc.whereat.activities;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,22 +12,54 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import org.tlc.whereat.R;
+import org.tlc.whereat.modules.LocationProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements LocationProvider.LocationCallback {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private LocationProvider mLocationProvider;
+    private Boolean mPollingOwnLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationProvider = new LocationProvider(this, this);
+        mLocationProvider.connect();
+        mPollingOwnLocation = false;
 
         final Button shareLocationButton = (Button) findViewById(R.id.go_button);
         shareLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toastLocationShared();
+                mLocationProvider.getLocation();
             }
         });
+        shareLocationButton.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v){
+                if (!mPollingOwnLocation){
+                    v.setBackground(getResources().getDrawable(R.drawable.go_button_on));
+                    mLocationProvider.pollLocation();
+                    mPollingOwnLocation = true;
 
+                }
+                else {
+                    v.setBackground(getResources().getDrawable(R.drawable.go_button_off));
+                    mLocationProvider.stopPollingLocation();
+                    mPollingOwnLocation = false;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mLocationProvider.disconnect();
     }
 
     @Override
@@ -37,16 +71,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { // The action bar will automatically handle clicks on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         switch(item.getItemId()){
-            case R.id.action_main:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
             case R.id.action_map:
                 startActivity(new Intent(this, MapActivity.class));
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void handleNewLocation(Location loc){
+        Log.i(TAG, "Received own location: " + loc.toString());
+        toastLocation(loc);
+    }
+
+    public void handleFailedLocationRequest(String msg) {
+        Toast.makeText(
+                getApplicationContext(), msg, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void toastLocation(Location loc){
+        Toast.makeText(
+                getApplicationContext(), "Location shared: " + loc.toString(), Toast.LENGTH_SHORT)
+                .show();
     }
 
     private void toastLocationShared(){
         Toast.makeText(getApplicationContext(), "Location shared.", Toast.LENGTH_SHORT).show();
     }
+
 }
---
+++ app/src/main/java/org/tlc/whereat/modules/LocationProvider.java
@@ -0,0 +1,119 @@
+package org.tlc.whereat.modules;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+
+public class LocationProvider implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    public interface LocationCallback {
+        void handleNewLocation(Location loc);
+        void handleFailedLocationRequest(String msg);
+    }
+
+    public static final String TAG = LocationProvider.class.getSimpleName();
+    public static final int INTERVAL = 5 * 1000; // 60 seconds
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000; //TODO: move to resources
+
+    private LocationCallback mLocationCallback;
+    private Context mContext;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    // Constructor
+    public LocationProvider(Context ctx, LocationCallback cb) {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(ctx)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(INTERVAL)
+                .setFastestInterval(INTERVAL);
+
+        mLocationCallback = cb;
+
+        mContext = ctx;
+    }
+
+    // Public Methods
+
+    public void connect(){
+        mGoogleApiClient.connect();
+    }
+
+    public void disconnect(){
+        if (mGoogleApiClient.isConnected()){
+           mGoogleApiClient.disconnect();
+        }
+    }
+
+    public void getLocation(){
+        //TODO: is there a more functional way to handle unhappy paths (client not connected loc is null)?
+        if (mGoogleApiClient.isConnected()){
+            Location loc = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (loc != null) mLocationCallback.handleNewLocation(loc);
+            else mLocationCallback.handleFailedLocationRequest("Location request failed");
+        }
+        else mLocationCallback.handleFailedLocationRequest("Location Services not connected.");
+    }
+
+    public void pollLocation(){
+        Log.i(TAG, "Passive location sharing turned on.");
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    public void stopPollingLocation(){
+        Log.i(TAG, "Passive location sharing turned off.");
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    // Location API Callbacks
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution() && mContext instanceof Activity) {
+            try {
+                Activity activity = (Activity)mContext;
+                connectionResult.startResolutionForResult(activity, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    // Location Listener Callbacks
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocationCallback.handleNewLocation(location);
+    }
+
+}
