ecbc03625fad90f66f12596cadd948a5b91e6e0a
aguestuser <aguestuser@riseup.net>
2015-07-01 15:53:50 -0400

    Add Dao class

---
+++ app/src/main/java/org/tlc/whereat/db/Dao.java
@@ -0,0 +1,48 @@
+package org.tlc.whereat.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class Dao extends SQLiteOpenHelper {
+
+    // FIELDS
+
+    public static final String TABLE_LOCATIONS = "locations";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_LAT = "lat";
+    public static final String COLUMN_LON = "lon";
+    public static final String COLUMN_TIME = "time";
+
+    private static final String DB_NAME = "whereat.db";
+    private static final int DB_VERSION = 1;
+
+    private static final String DB_CREATE =
+        "create table" + TABLE_LOCATIONS + "(" +
+            COLUMN_ID + " integer primary key autoincrement, " +
+            COLUMN_LAT + " real not null" +
+            COLUMN_LON + "real not null" +
+            COLUMN_TIME + "text not null);";
+
+    // CONSTRUCTOR
+
+    public Dao(Context ctx){
+        super(ctx, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DB_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(
+            Dao.class.getName(),
+            "Upgrading DB from v. " + oldVersion + " to " + newVersion + "; will destroy all old data"
+        );
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOCATIONS);
+        onCreate(db);
+    }
+}
