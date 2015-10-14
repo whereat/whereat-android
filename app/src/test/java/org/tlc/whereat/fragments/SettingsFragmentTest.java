e942e533a68f5ec86df98376f0651e30eab95f91
aguestuser <aguestuser@riseup.net>
2015-10-14 16:56:00 -0400

    Add preference list for location ttl
    * Options: (30 min, 1 hr, 2 hr)
    * Pref summary updates when pref changes

--- app/src/test/java/org/tlc/whereat/fragments/SettingsFragmentTest.java
+++ app/src/test/java/org/tlc/whereat/fragments/SettingsFragmentTest.java
@@ -43,7 +43,19 @@ public class SettingsFragmentTest {
         ctx.getString(R.string.pref_loc_share_interval_label_3),
         ctx.getString(R.string.pref_loc_share_interval_label_4)
     };
+    
+    static CharSequence[] locTtlVals = {
+        ctx.getString(R.string.pref_loc_ttl_value_0),
+        ctx.getString(R.string.pref_loc_ttl_value_1),
+        ctx.getString(R.string.pref_loc_ttl_value_2),
+    };
 
+    static CharSequence[] locTtlLabels = {
+        ctx.getString(R.string.pref_loc_ttl_label_0),
+        ctx.getString(R.string.pref_loc_ttl_label_1),
+        ctx.getString(R.string.pref_loc_ttl_label_2),
+    };
+    
 
     @Before
     public void setup() {
@@ -90,6 +102,37 @@ public class SettingsFragmentTest {
 
         setListPref(R.string.pref_loc_share_interval_value_4);
         assertThat(lp.getSummary()).isEqualTo(frag.getString(R.string.pref_loc_share_interval_label_4));
+    }
+
+    @Test
+    public void locTtlPref_should_haveCorrectDefaultContents(){
+        lp = findListPref(R.string.pref_loc_ttl_key);
+
+        assertThat(lp).isNotNull();
+        assertThat(lp.isPersistent()).isTrue();
+
+        assertThat(lp.getTitle()).isEqualTo(frag.getString(R.string.pref_loc_ttl_title));
+        assertThat(lp.getSummary()).isEqualTo(frag.getString(R.string.pref_loc_ttl_label_1));
+
+        assertThat(lp.getEntries()).isEqualTo(locTtlLabels);
+        assertThat(lp.getEntryValues()).isEqualTo(locTtlVals);
+
+        assertThat(lp.getEntry()).isEqualTo(frag.getString(R.string.pref_loc_ttl_label_1));
+        assertThat(lp.getValue()).isEqualTo(frag.getString(R.string.pref_loc_ttl_value_1));
+    }
+
+    @Test
+    public void locTtlPref_should_updateSummaryWhenPrefChanges(){
+        lp = findListPref(R.string.pref_loc_ttl_key);
+
+        setListPref(R.string.pref_loc_ttl_value_0);
+        assertThat(lp.getSummary()).isEqualTo(frag.getString(R.string.pref_loc_ttl_label_0));
+
+        setListPref(R.string.pref_loc_ttl_value_1);
+        assertThat(lp.getSummary()).isEqualTo(frag.getString(R.string.pref_loc_ttl_label_1));
+
+        setListPref(R.string.pref_loc_ttl_value_2);
+        assertThat(lp.getSummary()).isEqualTo(frag.getString(R.string.pref_loc_ttl_label_2));
     }
 
     protected ListPreference findListPref(int id){
